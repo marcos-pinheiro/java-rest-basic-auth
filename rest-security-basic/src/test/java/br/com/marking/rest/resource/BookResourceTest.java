d853882f27ab02cb8fc69b58e3d5f2f5a67d8496
Marcos Pinheiro <>
2015-08-02 22:22:28 -0300

    Adição do método resolveTemplate no case de testes

--- rest-security-basic/src/test/java/br/com/marking/rest/resource/BookResourceTest.java
+++ rest-security-basic/src/test/java/br/com/marking/rest/resource/BookResourceTest.java
@@ -22,7 +22,7 @@ import static org.junit.Assert.*;
  */
 public class BookResourceTest {
     
-    private static final String TARGET      = "http://localhost:8080/rest-security-basic/rest";
+    private static final String TARGET      = "http://localhost:8080/rest-security-basic/rest/books";
     private static final String USERNAME    = "admin";
     private static final String PASSWORD    = "java";
 
@@ -32,7 +32,8 @@ public class BookResourceTest {
         Client client = ClientBuilder.newClient();
         Book book = client
                 .target(TARGET)
-                .path("/books/0321356683")
+                .path("/{isbn}")
+                .resolveTemplate("isbn", "0321356683")
                 .register(newAuthenticator(USERNAME, PASSWORD))
                 .request()
                 .get(Book.class);
@@ -41,13 +42,12 @@ public class BookResourceTest {
         client.close();
     }
 
-    @Test
+   @Test
     public void testRegister() {
         
         Client client = ClientBuilder.newClient();
         Response firstResponse = client
                 .target(TARGET)
-                .path("/books")
                 //.register(newAuthenticator(USERNAME, PASSWORD)) -> BASIC Necessário somente nos métodos PUT, GET e DELETE segundo a configuração no web.xml
                 .request()
                 .post(Entity.json(
@@ -68,13 +68,14 @@ public class BookResourceTest {
         client.close();
     }
 
-    @Test
+   @Test
     public void testUpdate() {
         
         Client client = ClientBuilder.newClient();
         Response response = client
                 .target(TARGET)
-                .path("/books/0987654321")
+                .path("/{isbn}")
+                .resolveTemplate("isbn", "0987654321")
                 .register(newAuthenticator(USERNAME, PASSWORD))
                 .request()
                 .put(Entity.json(new BookDTO("REST - Construa APIs inteligentes de maneira simples", new BigDecimal("59.90"))));
@@ -91,7 +92,8 @@ public class BookResourceTest {
         Client client = ClientBuilder.newClient();
         Response response = client
                 .target(TARGET)
-                .path("/books/9999999999")
+                .path("/{isbn}")
+                .resolveTemplate("isbn", "9999999999")
                 .register(newAuthenticator(USERNAME, PASSWORD))
                 .request()
                 .delete();
@@ -106,7 +108,6 @@ public class BookResourceTest {
         Client client = ClientBuilder.newClient();
         List<Book> books = client
                 .target(TARGET)
-                .path("/books")
                 .queryParam("description", "JAVA")
                 .register(newAuthenticator(USERNAME, PASSWORD))
                 .request()
